017c6cc3cb21824779d4f58e9e5434b407aaaf80
kjforfun <o912779665@gmail.com>
2026-06-07 04:19:41 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 fetch('combos.json')
-.then(r=>r.json())
-.then(data=>{
-Analysis.init(data);
-UI.render();
-});
+  .then(r => r.json())
+  .then(data => {
+
+    Analysis.init(data);
+
+    document.dispatchEvent(
+      new Event('dataLoaded')
+    );
+
+  });
